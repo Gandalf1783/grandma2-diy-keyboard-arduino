948d37cba4c96acaf5b3b129de4715267b2055a1
Gandalf1783 <46635221+Gandalf1783@users.noreply.github.com>
2020-04-10 01:47:39 +0200

    Update grandMA2Keyboard.ino

--- grandMA2Keyboard.ino
+++ grandMA2Keyboard.ino
@@ -1,349 +1,62 @@
-#include <Adafruit_NeoPixel.h>
 #include <Keypad.h>
 
-#define LED_PIN    6
-
-// How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 1
-
-// Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 500
-
-uint32_t default_high = strip.Color(224, 114, 0);
-uint32_t default_color = strip.Color(122, 78, 0);
-const byte ROWS = 4;
-const byte COLS = 4;
-char hexaKeys[ROWS][COLS] = {
+char hexaKeys[4][4] = {
   {'0', '1', '2', '3'},
   {'4', '5', '6', '7'},
   {'8', '9', 'A', 'B'},
   {'C', 'D', 'E', 'F'},
 };
-byte colPins[COLS] = {31, 33, 35, 37};
-byte rowPins[ROWS] = {36, 34, 32, 30};
-byte colPins2[COLS] = {39, 41, 43, 45};
-byte rowPins2[ROWS] = {44, 42, 40, 38};
-const int COUNT = ROWS * COLS;
-boolean button[COUNT] = {
-  false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false
-};
+//Pins for keypad 1
+byte colPins[4] = {31, 33, 35, 37};
+byte rowPins[4] = {36, 34, 32, 30};
+//Pins for keypad 2
+byte colPins2[4] = {39, 41, 43, 45};
+byte rowPins2[4] = {44, 42, 40, 38};
 
-boolean key1 = false;
-boolean key2 = false;
-
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
-Keypad customKeypad2 = Keypad( makeKeymap(hexaKeys), rowPins2, colPins2, ROWS, COLS);
+//Creating Keypads
+Keypad keypad1 = Keypad( makeKeymap(hexaKeys), rowPins, colPins, 4, 4);
+Keypad keypad2 = Keypad( makeKeymap(hexaKeys), rowPins2, colPins2, 4, 4);
 
 void setup() {
+  //Setting up LEDs
+  pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
+  //Setting RUN LED to HIGH
+  digitalWrite(11, HIGH);
+  
+  //Beginning Serial on Baudrate 115200
   Serial.begin(115200);
-  Serial.println("grandMA2 onPC Controller Ver.1.0");
-  Serial.println("Initialising Devices...");
+  Serial.println("grandMA2 onPC MIDI<->Controller Software");
+  Serial.println("created by Gandalf1783");
+  //Turning on all LEDs after another
+  for (int i = 1; i < 4; i++) {
+    int j = i + 10;
+    digitalWrite(j, HIGH);
+    delay(250);
+  }
+  //Turning off all LEDs after another
+  for (int i = 1; i < 4; i++) {
+    int j = i + 10;
+    digitalWrite(j, LOW);
+    delay(250);
+  }
 
-  //INIT DEVICES HERE
+  Serial.println("Loading...");
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  strip.begin();
-  strip.show();
 
-  digitalWrite(12, HIGH);
-
-  strip.clear();
-  //Coloring Strip to default - Preparing for default operation
-  strip.fill(default_high, 0, LED_COUNT);
-  strip.show();
-
-  waitForTestMode();
-
-  Serial.println("Devices configured.");
-  Serial.println("Starting...");
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(11, LOW);
-  delay(1000);
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(11, LOW);
-  delay(1000);
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(11, LOW);
-  delay(1000);
-  Serial.println("Ready.");
-
+  Serial.println("Done.");
 }
 
 void loop() {
-  char customKey = customKeypad.getKey();
-  char customKey2 = customKeypad2.getKey();
-  if (customKey) {
-    digitalWrite(12, LOW);
-  }
-  if (customKey2) {
-    digitalWrite(12, LOW);
-  }
-  delay(40);
-  digitalWrite(12, HIGH);
-  sendFirstMIDI(customKey);
-  sendSecondMIDI(customKey2);
+  char customKey = keypad1.getKey();
+  char customKey2 = keypad2.getKey();
+  sendKeys1(customKey);
+  sendKeys2(customKey2);
 }
-
-void waitForTestMode() {
-  uint32_t period = 5 * 600L;       // 5 minutes
-  for ( uint32_t tStart = millis();  (millis() - tStart) < period;  ) {
-    char customKey = customKeypad.getKey();
-    char customKey2 = customKeypad2.getKey();
-
-    if (customKey) {
-      switch (customKey) {
-        case 'F':
-          key1 = true;
-          break;
-      }
-    }
-    if (customKey2) {
-      switch (customKey2) {
-        case 'F':
-          key2 = true;
-          break;
-      }
-    }
-
-    if (key1 && key2) {
-
-      Serial.println("TEST MODE");
-      Serial.println("Setting up TestMode enviroment...");
-      //Turning off LEDS
-      digitalWrite(LED_BUILTIN, LOW);
-      strip.clear();
-      strip.show();
-      //Array of booleans, index turns true if the button is pressed.
-      boolean keypad1[16] = {
-        false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false
-      };
-      boolean keypad2[16] = {
-        false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false
-      };
-
-      delay(100);
-      Serial.println("Press all keys. Reset Board or re-insert to exit Testmode.");
-      //Repeating forever, waiting for keypresses
-
-      while (true) {
-        char customKey = customKeypad.getKey();
-        char customKey2 = customKeypad2.getKey();
-        if (customKey) {
-          digitalWrite(12, LOW);
-          Serial.println("Keyboard 1 was pressed");
-          switch (customKey) {
-            case '0':
-              keypad1[0] = true;
-            case '1':
-              keypad1[1] = true;
-              break;
-            case '2':
-              keypad1[2] = true;
-              break;
-            case '3':
-              keypad1[3] = true;
-              break;
-            case '4':
-              keypad1[4] = true;
-              break;
-            case '5':
-              keypad1[5] = true;
-              break;
-            case '6':
-              keypad1[6] = true;
-              break;
-            case '7':
-              keypad1[7] = true;
-              break;
-            case '8':
-              keypad1[8] = true;
-              break;
-            case '9':
-              keypad1[9] = true;
-              break;
-            case 'A':
-              keypad1[10] = true;
-              break;
-            case 'B':
-              keypad1[11] = true;
-              break;
-            case 'C':
-              keypad1[12] = true;
-              break;
-            case 'D':
-              keypad1[13] = true;
-              break;
-            case 'E':
-              keypad1[14] = true;
-              break;
-            case 'F':
-              keypad1[15] = true;
-              break;
-          }
-        }
-        if (customKey2) {
-          digitalWrite(12, LOW);
-          Serial.println("Keyboard 2 was pressed");
-          switch (customKey2) {
-            case '0':
-              keypad2[0] = true;
-              break;
-            case '1':
-              keypad2[1] = true;
-              break;
-            case '2':
-              keypad2[2] = true;
-              break;
-            case '3':
-              keypad2[3] = true;
-              break;
-            case '4':
-              keypad2[4] = true;
-              break;
-            case '5':
-              keypad2[5] = true;
-              break;
-            case '6':
-              keypad2[6] = true;
-              break;
-            case '7':
-              keypad2[7] = true;
-              break;
-            case '8':
-              keypad2[8] = true;
-              break;
-            case '9':
-              keypad2[9] = true;
-              break;
-            case 'A':
-              keypad2[10] = true;
-              break;
-            case 'B':
-              keypad2[11] = true;
-              break;
-            case 'C':
-              keypad2[12] = true;
-              break;
-            case 'D':
-              keypad2[13] = true;
-              break;
-            case 'E':
-              keypad2[14] = true;
-              break;
-            case 'F':
-              keypad2[15] = true;
-              break;
-          }
-        }
-        //Setting up testComplete
-        bool testComplete = true;
-        //Checking if every key on keypad1 was pressed
-        for (bool temp_bool : keypad1) {
-          if (!temp_bool) {
-            //setting testComplete to false when not
-            testComplete = false;
-          }
-        }
-        //Checking if every key on keypad 2 was pressed
-        for (bool temp_bool : keypad2) {
-          if (!temp_bool) {
-            //setting testComplete to false when not
-            testComplete = false;
-          }
-        }
-        //When testComplete = true, while loop will exit and continue
-        if (testComplete) {
-          Serial.println("Button test complete...");
-          break;
-        }
-        delay(40);
-        digitalWrite(12, HIGH);
-      } // END WHILE LOOP
-
-      Serial.println("Turning on all LEDs. If ready, press any key.");
-      delay(100);
-
-      //Setting strip to white.
-      strip.clear();
-      uint32_t white = strip.Color(255, 255, 255);
-      strip.fill(white, 0, LED_COUNT);
-      strip.show();
-      //Turning on onBoard LED
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(12, HIGH);
-
-      while (true) {
-        char customKey = customKeypad.getKey();
-        char customKey2 = customKeypad2.getKey();
-        //Wait until any key is pressed, if yes, break the loop.
-        if (customKey || customKey2) {
-          break;
-        }
-      }
-      // Setting onBoard LED to off, and turning off LED-Strip
-      digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(12, LOW);
-      delay(500);
-      Serial.println("Turning off all LEDs. If ready, press any key.");
-      strip.clear();
-      strip.show();
-
-      while (true) {
-        char customKey = customKeypad.getKey();
-        char customKey2 = customKeypad2.getKey();
-        //Waiting until any key is pressed, if yes, break the loop
-        if (customKey || customKey2) {
-          break;
-        }
-      }
-      Serial.println("Done testing. Device is now under normal operation.");
-
-      //Unsetting vars
-      key1 = false;
-      key2 = false;
-      //Ending test program
-    } else {
-      key1 = false;
-      key2 = false;
-    }
-  }
-}
-
-
-
-
-
-
-// ONLY MIDI SEND FUNCTIONS!!!
-
-
-
-
-
-
-// NOTHING TO MODIFY HERE!
-
-void sendFirstMIDI(char customKey) {
+void sendKeys1(char customKey) {
   if (customKey) {
-    switch (customKey) {
+        switch (customKey) {
       case '0':
         Serial.write(B10010000);
         Serial.write(1);
@@ -440,10 +153,9 @@ void sendFirstMIDI(char customKey) {
   }
 }
 
-void sendSecondMIDI(char customKey2) {
-
-  if (customKey2) {
-    switch (customKey2) {
+void sendKeys2(char customKey) {
+  if (customKey) {
+         switch (customKey) {
       case '0':
         Serial.write(B10010000);
         Serial.write(17);
@@ -588,6 +300,6 @@ void sendSecondMIDI(char customKey2) {
         Serial.write(32);
         Serial.write(0);
         break;
-    }
+    } 
   }
 }
